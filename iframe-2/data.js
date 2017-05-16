5c595d7df96bda2f5f5342fe272a72547aadb42b
tittarunt360 <tittarunt360@gmail.com>
2017-05-16 21:53:33 +0200

    iframe 2 test

---
+++ iframe-2/data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-panorama-matador-seo",
+      "name": "360-panorama-matador-seo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 256,
+      "initialViewParameters": {
+        "yaw": 0.3580486266617964,
+        "pitch": 0,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
